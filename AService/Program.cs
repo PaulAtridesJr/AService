1cc399a8fac3abadbaba4ac49ba054053511d336
Timofey Chudov <TchoudovTI@ekassir.com>
2023-09-05 23:16:14 +0300

    Add project files.

---
+++ AService/Program.cs
@@ -0,0 +1,15 @@
+namespace AService
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var app = builder.Build();
+
+			app.MapGet("/", () => "Hello World!");
+
+			app.Run();
+		}
+	}
+}
